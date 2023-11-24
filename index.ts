fa76585393833c95aafe52c161d3312634275253
Son Do <son@chillbits.com>
2023-11-24 04:33:09 +0000

    Init

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { TonClient, WalletContractV4, internal } from "ton";
+import { mnemonicNew, mnemonicToPrivateKey } from "ton-crypto";
+
+// Create Client
+const client = new TonClient({
+  endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+});
+
+async function main() {
+  // Generate new key
+  let mnemonics = 'rug basket universe potato pledge nuclear style office coin permit purse face arena sadness update price affair stock coral minimum glance furnace october intact';
+  let keyPair = await mnemonicToPrivateKey(mnemonics.split(' '));
+
+// Create wallet contract
+  let workchain = 0; // Usually you need a workchain 0
+  let wallet = WalletContractV4.create({ workchain, publicKey: keyPair.publicKey });
+  let contract = client.open(wallet);
+
+// Get balance
+  let balance: bigint = await contract.getBalance();
+
+  console.log(balance);
+}
+
+main();
